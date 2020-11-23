96f21324cf575eef11afb390153b5d915058031e
“Hunter <hunterlmcdaniel@ufl.edu>
2020-11-22 23:35:12 -0500

    attempt 1

---
+++ main.js
@@ -0,0 +1,6 @@
+var random ["Hey", "Bonjour", "Hola", "Welcome", "Greetings", "Hiya!", "Hey, bud", "Sup?", "Hello", "Hey party people"]
+
+document.getElementById('mybutton').addEventListener('click', ()=>{
+            this.random();
+            console.log('pressed');
+        });
