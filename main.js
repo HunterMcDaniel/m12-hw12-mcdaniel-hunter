377fe237affbeda9f806884ff8d55890c4898d9e
“Hunter <hunterlmcdaniel@ufl.edu>
2020-11-23 23:07:33 -0500

    add css/ fin

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-var random = ["Hey" , "Bonjour" , "Hola" , "Welcome" , "Greetings" , "Hiya!" , "Hey, bud" , "Sup?" , "Hello" , "Hey party people"]
+var myList = [
+"Hey" ,
+"Bonjour" ,
+"Hola" ,
+"Welcome" ,
+"Greetings" ,
+"Hiya!" ,
+"Hey, bud" ,
+"Sup?" ,
+"Hello" ,
+"Hey party people"
+]
+
+var randomItem = myList[Math.floor(Math.random()*myList.length)];
 
 document.getElementById('mybutton').addEventListener('click', ()=>{
             console.log('random');
-            document.getElementById('fill').innerHTML = random;
+            document.getElementById('fill').innerHTML = randomItem;
         });
